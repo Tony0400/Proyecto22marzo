a57a480a27dddaf0dfea430d813c9b52c715edfc
mandp9 <MPilar.Ballesteros3@alu.uclm.es>
2023-03-21 22:33:04 +0100

    v2

--- src/g5/Hub.java
+++ src/g5/Hub.java
@@ -1,4 +1,74 @@
 package g5;
 
 public class Hub {
+    private Contenedor[][] cc;
+
+    public Hub() {
+        this.cc = new Contenedor[10][12];
+    }
+
+    public Contenedor[][] getArrayCont() {
+        return cc;
+    }
+
+    public void setArrayCont(Contenedor[][] cc) {
+        this.cc = cc;
+    }
+
+    public String toString() {
+        String s = "";
+        for (int i = 0; i < cc.length; i++) {
+            for (int j = 0; j < cc[i].length; j++) {
+                if (cc[i][j] == null) {
+                    s += "[Null]";
+                } else {
+                    s += "[C" + cc[i][j].getId() + "]";
+                }
+            }
+            s += "\n";
+        }
+        return s;
+    }
+    public Contenedor [][] apilar (Contenedor c) {
+        boolean stop = false;
+        if (c != null) {
+            int n = c.getPrioridad();
+            int col = n - 1;
+            for (int i = cc.length - 1; i >= 0 && !stop; i--) {
+                if (cc[i][col] == null) {
+                    cc[i][col] = c;
+                    stop = true;
+                }
+            }
+            if (n == 3 && !stop) {
+                for (int i = cc.length - 1; i >= 0; i--) {
+                    for (int j = 3; j < cc[i].length && !stop; j++) {
+                        if (cc[i][j] == null) {
+                            cc[i][j] = c;
+                            stop = true;
+                        }
+                    }
+                }
+            }
+        }
+        return cc;
+    }
+    public Contenedor [][] desapilar(int columna) {
+        int num;
+        String s = "";
+        int contador = 0;
+        if (columna >= 0) {
+            for (int i=0; i<cc.length; i++) {
+                //recorre el hub buscando los contenedores de esa columna que no estén vacíos.
+                if (cc[i][columna] != null) {
+                    cc[i][columna] = null;
+                    contador ++;
+                    s+= i + " ";
+                }
+            }
+            System.out.println("Se han encontrado los siguientes contenedores llenos en la filas: " + s + ". Procedemos a vaciarlos.");
+            System.out.println("Se han vaciado un total de: " + contador + " contenedores");
+        }
+        return cc;
+    }
 }
